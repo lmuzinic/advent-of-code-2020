923706bf6e98963127b0e2628c99e28d752c3dab
Luka Muzinic <luka@muzinic.net>
2020-12-02 11:47:52 +0100

    Day 2

---
+++ src/day2.rs
@@ -0,0 +1,74 @@
+use crate::Err;
+use regex::Regex;
+use std::ops::Index;
+use std::str::FromStr;
+
+pub fn first(input: &[String]) -> Result<u32, Err> {
+    let mut valid_password_count: u32 = 0;
+    for o in input {
+        if let Ok(parsed_input) = Input::from_str(o) {
+            let password: Vec<char> = parsed_input.password.chars().collect();
+
+            let mut count: u32 = 0;
+            for p in password {
+                if p == parsed_input.letter {
+                    count += 1;
+                }
+            }
+
+            if count >= parsed_input.min && count <= parsed_input.max {
+                valid_password_count += 1;
+            }
+        }
+    }
+
+    Ok(valid_password_count)
+}
+
+pub fn second(input: &[String]) -> Result<u32, Err> {
+    let mut valid_password_count: u32 = 0;
+
+    for o in input {
+        if let Ok(parsed_input) = Input::from_str(o) {
+            let password: Vec<char> = parsed_input.password.chars().collect();
+
+            if (password[(parsed_input.min - 1) as usize] == parsed_input.letter
+                && password[(parsed_input.max - 1) as usize] != parsed_input.letter)
+                || (password[(parsed_input.min - 1) as usize] != parsed_input.letter
+                    && password[(parsed_input.max - 1) as usize] == parsed_input.letter)
+            {
+                valid_password_count += 1;
+            }
+        }
+    }
+
+    Ok(valid_password_count)
+}
+
+#[derive(Debug)]
+struct Input {
+    pub min: u32,
+    pub max: u32,
+    pub letter: char,
+    pub password: String,
+}
+
+impl FromStr for Input {
+    type Err = crate::Err;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)").unwrap();
+        if let Some(c) = re.captures(input) {
+            let i = Input {
+                min: c.index(1).parse::<u32>().unwrap(),
+                max: c.index(2).parse::<u32>().unwrap(),
+                letter: c.index(3).parse::<char>().unwrap(),
+                password: c.index(4).parse::<String>().unwrap(),
+            };
+
+            return Ok(i);
+        }
+
+        Err(Err::Failed())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod day1;
+mod day2;
 
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 pub enum Err {
-    Failed()
+    Failed(),
 }
 
 fn main() {
@@ -18,16 +19,28 @@ fn main() {
     if let Ok(result) = day1::second(&account) {
         println!("Day 1, part 2: {}", result);
     }
+
+    let passwords = input_vec::<String>("./input/day2.txt");
+
+    if let Ok(result) = day2::first(&passwords) {
+        println!("Day 2, part 1: {}", result);
+    }
+
+    if let Ok(result) = day2::second(&passwords) {
+        println!("Day 2, part 2: {}", result);
+    }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
 fn input_vec<T: std::str::FromStr>(filename: &str) -> Vec<T> {
-    let mut input:Vec<T> = Vec::new();
+    let mut input: Vec<T> = Vec::new();
 
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
